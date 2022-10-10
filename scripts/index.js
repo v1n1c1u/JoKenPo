e952d59774a0d9d9f94084d4e28c3ecea88ca81e
v1n1c1u <vinicius.souza@ccc.ufcg.edu.br>
2022-10-10 20:28:42 -0300

    Optmizing the code

--- scripts/index.js
+++ scripts/index.js
@@ -15,35 +15,41 @@ const computerPlay = () => {
         case 3:
             return "paper"
         default:
-            return `Oh no! Something went wrong!`
+            return "apache helicopter"
     }
 }
 
-const playRound = (playerSelection=null, computerSelection=null) => {
-    if(playerSelection === SCISSORS && computerSelection === PAPER){
-        playerWins ++;
-        return "You Win! Scissors beats Paper"
-    }else if(playerSelection === SCISSORS && computerSelection === ROCK){
-        computerWins ++;
-        return "You Lose! Rock beats Scissors"
-    }else if(playerSelection === SCISSORS && computerSelection === SCISSORS){
-        return "It's a tie! Both chose scissors!"
-    }else if(playerSelection === PAPER && computerSelection === PAPER){
-        return "It's a tie! Both chose paper!"
-    }else if(playerSelection === PAPER && computerSelection === ROCK){
-        playerWins ++;
-        return "You Win! Paper beats Rock"
-    }else if(playerSelection === PAPER && computerSelection === SCISSORS){
-        computerWins ++;
-        return "You Lose! Scissors beats Paper"
-    }else if(playerSelection === ROCK && computerSelection === PAPER){
-        computerWins ++;
-        return "You Lose! Paper beats Rock"
-    }else if(playerSelection === ROCK && computerSelection === ROCK){
-        return "It's a tie! Both chose rock!"
-    }else if(playerSelection === ROCK && computerSelection === SCISSORS){
-        playerWins ++;
-        return "You Win! Rock beats Scissors"
+const playRound = (playerSelection, computerSelection) => {
+    if(playerSelection === SCISSORS){
+        if(computerSelection === PAPER){
+            playerWins ++;
+            return "You Win! Scissors beats Paper"
+        }else if(computerSelection === ROCK){
+            computerWins ++;
+            return "You Lose! Rock beats Scissors"
+        }else if(computerSelection === SCISSORS){
+            return "It's a tie! Both chose scissors!"
+        }
+    }else if(playerSelection === PAPER){
+        if(computerSelection === PAPER){
+            return "It's a tie! Both chose paper!"
+        }else if(computerSelection === ROCK){
+            playerWins ++;
+            return "You Win! Paper beats Rock"
+        }else if(computerSelection === SCISSORS){
+            computerWins ++;
+            return "You Lose! Scissors beats Paper"
+        }
+    }else if(playerSelection === ROCK){
+        if(computerSelection === PAPER){
+            computerWins ++;
+            return "You Lose! Paper beats Rock"
+        }else if(playerSelection === ROCK && computerSelection === ROCK){
+            return "It's a tie! Both chose rock!"
+        }else if(playerSelection === ROCK && computerSelection === SCISSORS){
+            playerWins ++;
+            return "You Win! Rock beats Scissors"
+        }
     }
 }
 
@@ -51,8 +57,8 @@ const choiceIsValid = (choice) => { return (choice === SCISSORS || choice === RO
 
 const game = () => {
     console.log("Let's play Jo Ken Po ! \n");
-    let computerSelection, playerSelection, validChoice, quit;
 
+    let computerSelection, playerSelection, validChoice, quit;
     computerWins = 0,
     playerWins = 0;
     quit = false;
@@ -81,18 +87,18 @@ const game = () => {
 
         console.log(`Your choice: ${playerSelection} \nComputer's choice: ${computerSelection}\n\n${playRound(playerSelection,computerSelection)}`);
     }
+    console.log("---------------- GAME OVER -------------------");
     if(quit){
-        console.log("---------------- GAME OVER -------------------");
         console.log(`            YOU QUIT! WHAT A LOSER!`)
     }else{
-        console.log("---------------- GAME OVER -------------------");
         var finalMessage = "";
+        const ties = 5-playerWins-computerWins;
         if(computerWins > playerWins){
-            finalMessage+= `YOU LOSE!\n \nComputer (${computerWins}) x You (${playerWins})  (${5-playerWins-computerWins} ties)`;
+            finalMessage+= `YOU LOSE!\n \nComputer (${computerWins}) x You (${playerWins})  (${ties} ties)`;
         }else if(playerWins > computerWins){
-            finalMessage+=`YOU WIN!\n \nYou (${playerWins}) x Computer (${computerWins})  (${5-playerWins-computerWins} ties)`;
+            finalMessage+=`YOU WIN!\n \nYou (${playerWins}) x Computer (${computerWins})  (${ties} ties)`;
         }else{
-            finalMessage+=`IT WAS A TIE!\n \nYou (${playerWins}) x Computer (${computerWins})  (${5-playerWins-computerWins} ties)`;
+            finalMessage+=`IT WAS A TIE!\n \nYou (${playerWins}) x Computer (${computerWins})  (${ties} ties)`;
         }
         finalMessage+="\n \n            Thanks for playing!";
     
